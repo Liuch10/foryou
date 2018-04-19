2389de75d3a0c718cdaeb6f6b461476cad6b1ac9
Cihang Liu <liuch10@gmail.com>
2018-04-20 01:14:49 +0800

    revisit the forms to fit the pages

--- app/utils/forms.py
+++ app/utils/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm as Form
-from wtforms.fields import StringField, SelectField, SubmitField, FileField
-from wtforms.validators import Required, Length
+from wtforms.fields import StringField, SelectField, SubmitField
+from wtforms.validators import Required, Length, NumberRange
 
 class LoginForm(Form):
     user_name           = StringField(validators = [Required(), Length(max = 15)])
@@ -19,10 +19,22 @@ class SignUpForm(Form):
     submit = SubmitField('Sign up')
 
 
-class uploadCaseForm(Form):
+class CaseForm(Form):
+    case_id = StringField()
     patient_name = StringField(validators=[Length(max=15)])
     patient_gender = SelectField(choices=[('0', '未知/保密'), ('1', '男'), ('2', '女')])
+    patient_age = StringField(validators=NumberRange(min=0, max=100))
     patient_photo_type = SelectField(
         choices=[('0', '其他'), ('1', 'CR'), ('2', 'DR'), ('3', 'CT'), ('4', 'MR'), ('5', '超声')])
     patient_photo_file = StringField(validators=[Required()])
+    is_diagnois = StringField()
+    is_expert = StringField()
+    patient_diagnois_type = SelectField(choices=[('0', '良性'), ('1', '恶性')])
+    patient_diagnois_result = SelectField(choices=[('0', '正常'), ('1', '一期'), ('2', '二期'), ('3', '三期')])
+    doctor_name = StringField()
+    doctor_hospital = StringField()
+    doctor_department = StringField()
+    case_upload_time = StringField()
+    case_diagnois_time = StringField()
+    case_expert_time = StringField()
     submit = SubmitField('Upload')
