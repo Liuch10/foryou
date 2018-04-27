4631a6cff736f0b2e6d6f49e998afa6d03814181
Cihang Liu <liuch10@gmail.com>
2018-04-27 22:31:30 +0800

    display image in the diagnose page

--- app/views/diagnoseCtrl.py
+++ app/views/diagnoseCtrl.py
@@ -1,4 +1,4 @@
-from flask import Response, render_template, jsonify, request, g
+from flask import Response, render_template, jsonify, request, g, redirect, url_for
 from app.models.models import User, Case, ExpertCase, Consultation
 from app.utils.forms import DiagnoseForm
 from app import db
@@ -11,9 +11,16 @@ def diagnose():
     case_id = 0
     request_case = Case.query.filter_by(id=case_id).first()
     g.user = current_user
+
+    ownership = True if (g.user.is_authenticated and int(g.user.id) == int(request_case.upload_user_id)) else False
+    # ownership = True
+
     if request.method == 'GET':
-        form = DiagnoseForm()
-        return render_template("diagnose.html", form=form)
+        if ownership:
+            form = DiagnoseForm()
+            return render_template("diagnose.html", form=form)
+        else:
+            return redirect(url_for('work'))
     elif request.method == "POST":
         ##TODO WRITE to DB
         return jsonify({'result': 'error'})
